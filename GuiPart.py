3db6935f26eb0f4c5d72119c71314ddc8219a908
Asheesh Mathur <asheesh@Asheeshs-MacBook-Pro.local>
2023-05-30 12:56:43 +0530

    Scroll Bar Related Fixes

--- GuiPart.py
+++ GuiPart.py
@@ -35,20 +35,24 @@ def generate():
     for row in aimg:
         scrolledText.insert(END, row + '\n')
         previewText.insert(END, row + '\n')
-    # scrolledText.config(text=convertedText)
-    # previewText.config(text=convertedText)
 
 
 # Define Four Frames
 # frame
 frameOne = ttk.Frame(root, width=800, height=55, borderwidth=5, relief=tk.GROOVE)
 frameTwo = ttk.Frame(root, width=800, height=50, borderwidth=5, relief=tk.GROOVE)
-frameThree = ttk.Frame(root, width=500, height=180, borderwidth=5, relief=tk.GROOVE)
+frameThree = ttk.Frame(root, width=650, height=180, borderwidth=5, relief=tk.GROOVE)
 frameFour = ttk.Frame(root, width=800, height=50, borderwidth=5, relief=tk.GROOVE)
-# Declare global variables
-scrolledText = st.Text(frameThree, font=('Courier', 14), wrap=NONE)
+
+# Add a Scrollbar(horizontal)
+hScrollBar = Scrollbar(frameThree, orient='horizontal')
+hScrollBar = Scrollbar(frameThree, orient='horizontal')
+scrolledText = st.ScrolledText(frameThree, font=('Courier', 14), wrap=NONE,xscrollcommand=hScrollBar.set)
+hScrollBar.config(command=scrolledText.xview)
+hScrollBar.pack(side=BOTTOM, fill='x')
 scrolledText.pack(side=LEFT, padx=25, pady=5)
-previewText = Text(frameThree, font=('Courier', 2), width=45, height=21, wrap=NONE)
+# Attach the scrollbar with the text widget
+previewText = st.ScrolledText(frameThree, font=('Courier', 2), width=45, height=21, wrap=NONE)
 previewText.pack(side=LEFT, padx=25, pady=5)
 # Buttons
 # imageTobeConverted = Button(frameOne, text="Generate", command=lambda: generate(imageFileName))
@@ -98,16 +102,13 @@ Image to ascii art display
 def asciiArt():
     global scrolledText, root, previewText
     root.mainloop()
-    # Add a Scrollbar(horizontal)
-    hScrollBar = Scrollbar(frameThree, orient='horizontal')
-    hScrollBar.pack(side=BOTTOM, fill='x')
+
     scrolledText.config(parent=frameThree, xscrollcommand=hScrollBar)
+    hScrollBar.config(command=scrolledText.xview)
     previewText.config(parent=frameThree)
     generateBtn.config(command=generateBtn)
     # Add a text widget
 
-    # Attach the scrollbar with the text widget
-    hScrollBar.config(command=previewText.xview)
 
 
 def main():
