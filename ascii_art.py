1f31e09de8e6dae179cac9a00655e237326ca88c
Asheesh Mathur <asheesh@Asheeshs-MacBook-Pro.local>
2023-06-02 19:43:07 +0530

    Resolved Display Issue with ACII Text

--- ascii_art.py
+++ ascii_art.py
@@ -185,31 +185,32 @@ fontSize = ttk.Label(frameOne, text="Font Size")
 fontSize.place(x=270, y=50)
 fontEntry = ttk.Entry(frameOne, width=2, textvariable=fntSize).place(x=330, y=45)
 
-# Widgets to accept set of characters
+# Widgets to accept set of characters for conversion
 listOfCharacters = ttk.Label(frameTwo, text="Input List of Characters to Used for Conversion - Preferably 70").grid(
     row=0, column=0, sticky=W)
 charListEntry = ttk.Entry(frameTwo, width=50, textvariable=asciiText).grid(row=1, column=0, sticky=W)
 
 hScrollBar = Scrollbar(frameThree, orient='horizontal')
-scrolledText = st.ScrolledText(frameThree, wrap=NONE, xscrollcommand=hScrollBar.set)
+scrolledText = st.ScrolledText(frameThree, wrap=NONE, width=95,xscrollcommand=hScrollBar.set)
 
 hScrollBar.config(command=scrolledText.xview)
 
 hScrollBar.grid(row=1, column=0, sticky=EW, padx=15)
 scrolledText.grid(row=0, column=0, padx=15, sticky=EW)
+
 # Attach the scrollbar with the text widget
 previewText = st.ScrolledText(frameThree, font=('Courier', 2), width=45, height=30, wrap=NONE)
 previewText.grid(row=0, column=1, sticky=E)
 
-previewLabel = ttk.Label(frameThree, text="Small Crisp Preview ").place(x=610, y=200)
+previewLabel = ttk.Label(frameThree, text=" Crisp Preview ").place(x=720, y=200)
+viewLabel = ttk.Label(frameThree, text="Crisp Preview").place(x=720, y=200)
 
-# Plan for Labels and entry fields
-
-# Buttons
+# Generate and Save Buttons
 generateBtn = Button(frameFour, text="Generate", command=lambda: generate())
 saveFileBtn = Button(frameFour, text="Save To", command=lambda: saveTo())
 generateBtn.place(x=325, y=5)
 saveFileBtn.place(x=420, y=5)
+
 # Pack these frames
 frameOne.pack(fill=BOTH, expand=True)
 frameTwo.pack(fill=BOTH, expand=True)
@@ -226,7 +227,7 @@ def saveTo():
 
 
 '''
-Image to ascii art display
+Rendering of ascii art display
 '''
 
 
